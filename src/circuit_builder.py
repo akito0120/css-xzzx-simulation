5587c639bf23904a5e1ee26eb64e14f07a00a411
Akito Yamaguchi <akito.ky.120@gmail.com>
2026-05-31 15:33:46 +0000

    Add clarification comments

--- src/circuit_builder.py
+++ src/circuit_builder.py
@@ -16,7 +16,6 @@ def build_circuit(code: QecCode, p: float, eta: float) -> stim.Circuit:
     # Memory: X
     
     px, py, pz = biased_pauli_rates(p, eta)
-
     circuit = stim.Circuit()
 
     # Initialize qubits
@@ -30,14 +29,16 @@ def build_circuit(code: QecCode, p: float, eta: float) -> stim.Circuit:
     circuit.append("R", data_list)
     if x_basis_data_list:
         circuit.append("H", x_basis_data_list)
+
     circuit.append("TICK")
 
     record_counter = 0
     current_round = 0
-    ancilla_record: Dict[Tuple[Coord, int], int] = {}
-    ancilla_order = list(code.stabilizers.keys())
+    ancilla_record: Dict[Tuple[Coord, int], int] = {} # (coordinate, round) -> absolute index
+    ancilla_order = list(code.stabilizers.keys()) # To fix the order of measurement
     
     def syndrome_meas():
+        # Measure ancillas and record the (coordinate, round) -> absolute index information
         nonlocal record_counter, current_round
         for ancilla in ancilla_order:
             ancilla_idx = code.ancilla_qubits[ancilla]
@@ -55,21 +56,24 @@ def build_circuit(code: QecCode, p: float, eta: float) -> stim.Circuit:
             ancilla_record[(ancilla, current_round)] = record_counter
             record_counter += 1
 
+    # Create the reference point of detector
+    # Map the state to the code space
     syndrome_meas()
+
     circuit.append("TICK")
 
     def rel(abs_idx):
-        nonlocal record_counter
         # Return the relative index of measurement result
+        nonlocal record_counter
         return stim.target_rec(abs_idx - record_counter)
 
     # Noisy round (1 round)
     current_round += 1
-
     if p > 0:
         circuit.append("PAULI_CHANNEL_1", data_list, [px, py, pz])
-    syndrome_meas()
 
+    # Syndrome extraction after noisy round
+    syndrome_meas()
     for ancilla in ancilla_order:
         d_now = ancilla_record[(ancilla, current_round)]
         d_prev = ancilla_record[(ancilla, current_round - 1)]
@@ -78,7 +82,6 @@ def build_circuit(code: QecCode, p: float, eta: float) -> stim.Circuit:
             [rel(d_now), rel(d_prev)],
             [ancilla[0], ancilla[1], current_round],
         )
-
     circuit.append("TICK")
 
     # Data readout
@@ -91,6 +94,7 @@ def build_circuit(code: QecCode, p: float, eta: float) -> stim.Circuit:
         data_record[dcoord] = record_counter
         record_counter += 1
 
+    # Final-round detector
     for ancilla in ancilla_order:
         legs = code.stabilizers[ancilla]
         if(all(pauli == ("Z" if dc in code.hset else "X") for dc, pauli in legs.items())):
@@ -98,6 +102,7 @@ def build_circuit(code: QecCode, p: float, eta: float) -> stim.Circuit:
             targets.append(rel(ancilla_record[(ancilla, 1)]))
             circuit.append("DETECTOR", targets, [ancilla[0], ancilla[1], 2])
     
+    # Define observable
     observable_targets = [rel(data_record[dc]) for dc in code.logical_x]
     circuit.append("OBSERVABLE_INCLUDE", observable_targets, 0)
 
